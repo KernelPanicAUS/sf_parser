c9b7a2c0ab3e8aec70de4d8399aaee7c672f8fd4
KernelPanicAUS <thomas@khalilt.com>
2013-12-03 08:38:05 +1100

    specs ot test parser

---
+++ spec/parser_spec.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+# encoding: UTF-8
+
+require_relative './spec_helper.rb'
+class Parser
+  describe "Script" do
+    it "should detect that the date is within the specified date range" do
+    end
+
+    it "should not count dates that are outside the specified date range" do
+    end
+
+    it "should not count a date in more than one date range" do
+    end
+  end
+end
+
+
