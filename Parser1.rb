c5a481b9da3ba5e8bc0400432c63f035806bb912
KernelPanicAUS <thomas@khalilt.com>
2013-12-04 10:26:18 +1100

    Cleaned up parser
    Added directory parsing functionality as well as column name parsing
    insead of static location

--- Parser1.rb
+++ Parser1.rb
@@ -37,12 +37,12 @@ end
 # Initialisers #
 ################
 log = File.open("./summary.log","a+")
-in_scope = File.open("./in_scope.log","w")
 rowcount = 0
 filecount = 0
-rangehash = {Date.parse("2012-01-01")=>Date.parse("2012-03-31"), Date.parse("2012-04-01")=>Date.parse("2012-06-30"), Date.parse("2012-07-01")=>Date.parse("2012-09-30"), Date.parse("2012-10-01")=>Date.parse("2012-12-31"),
-             Date.parse("2013-01-01")=>Date.parse("2013-03-31"), Date.parse("2013-04-01")=>Date.parse("2013-06-30"), Date.parse("2013-07-01")=>Date.parse("2013-09-30"), Date.parse("2013-10-01")=>Date.parse("2013-12-31")}
-counters = ["0","0","0","0","0","0","0","0"]
+results = Hash.new(0)
+filelist = Array.new
+
+ranges = [(Date.parse("2012-01-01")..Date.parse("2012-03-31")), (Date.parse("2012-04-01")..Date.parse("2012-06-30")), (Date.parse("2012-07-01")..Date.parse("2012-09-30")), (Date.parse("2012-10-01")..Date.parse("2012-12-31")), (Date.parse("2013-01-01")..Date.parse("2013-03-31")), (Date.parse("2013-04-01")..Date.parse("2013-06-30")), (Date.parse("2013-07-01")..Date.parse("2013-09-30")), (Date.parse("2013-10-01")..Date.parse("2013-12-31"))]
 
 # Each lines from the CSV will be fed to the script for evaluation and will be subsequently dumped from memory as the CSV files are quite large.
 # Each line will be delimited, the 4 th element which is the record creation date, will be evaluated against a few timeframes which are stored in the rangehash hash.
@@ -50,33 +50,38 @@ counters = ["0","0","0","0","0","0","0","0"]
 
 log.write("#{Time.now}\n")
 
-time = Benchmark.realtime do
-  CSV.with_progress_bar.foreach("/Users/Thomas/GitHub/SiteMinder/scripts/sf_parser/EmailMessage_utf8.csv",:headers => true) do |row|
-    rangehash.each_with_index do |(start_date, finish_date), index|
-      new_range = (start_date..finish_date)
-      dirty_date = Date.parse(row[4].split[0]) rescue nil
-      # in_scope.write("#{dirty_date.to_s},  #{start_date.to_s},  #{finish_date.to_s},  Index : #{index},  ")
-        if new_range.include?(dirty_date)
-          # rowcount+=1
-          counters[index] += 1
-          # in_scope.write("Yes,  counter : #{index},  value : #{counters[index]}\n")
-        else
-          # in_scope.write("No,  counter : #{index},  value : #{counters[index]}\n")
-        end
-
-    # in_scope.write("#{counters}")
-    end
-  filecount+=1
+# Compiles a list of CSV files in the script's current working directory.
+Dir.foreach(".") do |file|
+  if File.extname(file) == '.csv'
+    filelist << file
   end
 end
 
-# Cycles through the counter array to output the script's outcome.
-counters.each_with_index do |counter, index|
-  log.write("Range N: #{index+1}, has: #{counter} lines between #{rangehash.flatten[index*2]} and #{rangehash.flatten[(index*2)+1]}\n")
+# Parses every CSV file saved in the previous operation.
+filelist.each do |spreadsheet|
+  puts spreadsheet
+  log.write("Parsing #{spreadsheet} \n")
+  
+  time = Benchmark.realtime do
+    CSV.with_progress_bar.foreach(spreadsheet,:headers => true, encoding:'iso-8859-1:utf-8') do |row|
+      
+      dirty_date = Date.parse(row[CreatedDate].split[0]) rescue nil
+      
+      ranges.each do |range|
+        results[range] += 1 if range.include? dirty_date
+      end
+
+    filecount+=1
+    end
+    log.write("#{filecount} lines in #{spreadsheet} \n")
+    
+    results.each do |range, count|
+      log.write("#{count} lines found between #{range.first.to_s} and #{range.last.to_s} .\n")
+    end
+    
+    log.write("Time elapsed to process this file #{time/60} seconds \n\n")
+  end
+  filecount = 0
 end
 
-log.write("Time elapsed to process this file #{time/60} seconds""\n")
-log.write("#{filecount} lines in this file""\n\n\n")
 log.close
-in_scope.close
-puts "#{time/60}"
